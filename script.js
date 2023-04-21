5d62d98b789c0360631f5ea696bf1c02ab2d8c63
GabrielBruno7 <gabriel.bruno1w@gmail.com>
2023-04-21 08:24:36 -0300

    Criando o CRUD

--- script.js
+++ script.js
@@ -5,6 +5,62 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+
+const tempClient = {
+    os: "12313",
+    nome:"valjoreler",
+    pcNotebook:"Notebook",
+    descriçao:"Placa de video",
+    statuss:"PRONTO",
+}
+
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient));
+
+
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index, 1)
+    setLocalStorage(dbClient)
+}
+
+const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbClient[index] = client
+    setLocalStorage(dbClient)
+}
+
+const readClient = () => getLocalStorage()
+
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    dbClient.push (client)
+    setLocalStorage(dbClient)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
